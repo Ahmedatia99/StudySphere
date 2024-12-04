7812c84ea1c84d952d02dda9888373e41931d7dc
Ahmedatia99 <atyaa629@gmail.com>
2024-12-04 20:26:28 +0200

    responsive update

--- src/component/Courses/atoms/Defination.jsx
+++ src/component/Courses/atoms/Defination.jsx
@@ -1,7 +1,7 @@
 export default function Defination({ heading, paragraph }) {
   return (
     <main className=" mx-auto border-b-2 px-5">
-      <section className="py-20 flex md:flex-row mobile:flex-col justify-between">
+      <section className="py-20 flex md:flex-row mobile:flex-col justify-between gap-10">
         <h2 className="md:text-4xl mobile:text-3xl font-semibold max-w-xl leading-snug">
           {heading}
         </h2>
